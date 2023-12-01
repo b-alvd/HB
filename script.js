ec3080d870a463e0495895a9b481efd99182fe9a
b-alvd <145813964+b-alvd@users.noreply.github.com>
2023-12-01 14:50:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,366 @@
+
+
+const profil = [
+  {
+    id: 0,
+    nom: "ADEL",
+    prenom: "Idriss",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 1,
+    nom: "ALEVEQUE",
+    prenom: "Basile",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 2,
+    nom: "BEKKAI",
+    prenom: "Zohra",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 3,
+    nom: "BOSSUT",
+    prenom: "Anthony",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 4,
+    nom: "BYNENS",
+    prenom: "Florian",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 5,
+    nom: "COPPI",
+    prenom: "Daniel",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 6,
+    nom: "KARMAOUI",
+    prenom: "Lana",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 7,
+    nom: "LAPOV",
+    prenom: "Timothée",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 8,
+    nom: "LE PIOUFFLE",
+    prenom: "Joseph",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 9,
+    nom: "MINARY",
+    prenom: "Emma",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 10,
+    nom: "PEREIRA",
+    prenom: "Nathalia",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 11,
+    nom: "PORTA",
+    prenom: "Maxime",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 12,
+    nom: "RASOLOFO",
+    prenom: "Issac",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 13,
+    nom: "RAVIER",
+    prenom: "Robin",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 14,
+    nom: "SIMONEAU",
+    prenom: "Hugo",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  },
+  {
+    id: 15,
+    nom: "TITAH",
+    prenom: "El Mostafa",
+    accroche: "Votre Accroche",
+    description: "Déscription.",
+    expertise: ["Angular", "Javascript", "PHP", "", "", ""],
+    identifiant_linked: "Votre_identifiant_linked", /* -> https://www.linkedin.com/in/identifiant_linked*/
+    identifiant_github: "Votre_Pseudo", /* -> https://www.github.com/VotrePseudo */
+    link_CV: "",
+    link_downlaod: "",
+    link_portofolio: "",
+    hobbies: ["HOBBIES 1", "HOBBIES 2", "HOBBIES 3", "HOBBIES 4", "HOBBIES 5", "HOBBIES 6"]
+  }
+
+];
+
+function affiche(popup, id){
+  console.log(id); 
+  
+  let nom_prenom = document.getElementById('nom_prenom');  
+  nom_prenom.innerText  = profil[id].nom +' '+ profil[id].prenom;
+
+  let accroche = document.getElementById('accroche');  
+  accroche.innerText  = profil[id].accroche;
+
+  let description = document.getElementById('description');  
+  description.innerText  = profil[id].description;
+
+  let expertise = document.getElementById('expertise');  
+  expertise.innerText  = profil[id].expertise;
+
+  let identifiant_linked = document.getElementById('identifiant_linked');  
+  identifiant_linked.innerText  = profil[id].identifiant_linked;
+
+  let identifiant_github = document.getElementById('identifiant_github');  
+  identifiant_github.innerText  = profil[id].identifiant_github;
+
+  let link_CV = document.getElementById('link_CV');  
+  link_CV.innerText  = profil[id].link_CV;
+
+  let link_downlaod= document.getElementById('link_downlaod');  
+  link_downlaod.innerText  = profil[id].link_downlaod;
+
+  let link_portofolio = document.getElementById('link_portofolio');  
+  link_portofolio.innerText  = profil[id].link_portofolio;
+
+  let hobbies = document.getElementById('hobbies');  
+  hobbies.innerText  = profil[id].hobbies;
+  
+   
+  /*
+  popup.style.transition = 'transform 0.1s ease-in-out';
+
+  if (popup.style.transform === 'translate(100%, -50%)') {
+    popup.style.transform = 'translate(0%, -70%)';
+  } else {
+    popup.style.transform = 'translate(100%, -50%)';
+  }
+*/
+}
+
+function affichePopup(id) { 
+
+  let chemin ="./assets/personnages/" ;
+
+  let imageperso = document.getElementById('imageperso');
+  console.log(imageperso.src);
+
+  let nomfile = chemin + profil[id].prenom.toLowerCase()+'pixelastro.png';
+  console.log(nomfile);
+  imageperso.src = nomfile; 
+  console.log(imageperso.src);  
+
+  let nom_prenom = document.getElementById('nom_prenom');  
+  nom_prenom.innerText  = profil[id].nom +' '+ profil[id].prenom;
+
+  let accroche = document.getElementById('accroche');  
+  accroche.innerText  = profil[id].accroche;
+
+  let description = document.getElementById('description');  
+  description.innerText  = profil[id].description;
+
+  let expertise = document.getElementById('expertise');  
+  expertise.innerText  = profil[id].expertise;
+
+  let identifiant_linked = document.getElementById('identifiant_linked');  
+  identifiant_linked.innerText  = profil[id].identifiant_linked;
+
+  let identifiant_github = document.getElementById('identifiant_github');  
+  identifiant_github.innerText  = profil[id].identifiant_github;
+
+  let link_CV = document.getElementById('link_CV');  
+  link_CV.innerText  = profil[id].link_CV;
+
+  let link_downlaod= document.getElementById('link_downlaod');  
+  link_downlaod.innerText  = profil[id].link_downlaod;
+
+  let link_portofolio = document.getElementById('link_portofolio');  
+  link_portofolio.innerText  = profil[id].link_portofolio;
+
+  let hobbies = document.getElementById('hobbies');
+  hobbies.innerText  = profil[id].hobbies;
+
+  let id_popup = document.getElementById('id_popup');
+  id_popup.style.display = 'block';
+   /*
+  popup.style.transition = 'transform 0.1s ease-in-out';
+  if (popup.style.transform === 'translate(100%, -50%)') {
+    popup.style.transform = 'translate(-50%, -50%)';
+  } else {
+    popup.style.transform = 'translate(100%, -50%)';
+  }
+  */
+}
+
+for (let i = 0; i < profil.length; i++) {  
+    
+  // Sélèction des id_nom et id_prenom pour chaque profil
+  let id_nom = document.getElementById("nom_" + i);
+  let id_prenom = document.getElementById("prenom_" + i);
+
+  // Modification des valeurs des éléments pour chaque profil
+  id_nom.innerText = profil[i].nom;
+  id_prenom.innerText = profil[i].prenom;
+
+  
+  
+}
+console.log("Nom chargés")
+/*
+function affichePopup(id) {
+  // Afficher la popup correspondante
+  let popupId = 'popup_' + id;
+  let popup = document.getElementById(popupId);
+  popup.style.display = 'block';
+}
+function fermerPopup(id) {
+  // Fermer la popup correspondante
+  let popupId = 'popup_' + id;
+  let popup = document.getElementById(popupId);
+  popup.style.display = 'none';
+}*/
+/*
+function affichePopup() {
+  let id_popup = document.getElementById('id_popup');
+  id_popup.style.display = 'block';
+}
+*/
+function closeProfil() {
+  let id_popup = document.getElementById('id_popup');
+  id_popup.style.display = 'none';
+}
